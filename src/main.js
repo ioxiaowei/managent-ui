7c57dfdbe92ce5dc31e313a4e2b7177f7e432289
wangxiaowei <ioxiaowei@vip.163.com>
2021-08-03 11:29:47 +0800

    fix:bugs

--- src/main.js
+++ src/main.js
@@ -3,14 +3,24 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import api from './http'
+import i18n from './i18n'
+import store from './store'
+import global from '@/utils/global'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import 'font-awesome/css/font-awesome.min.css'
 
-Vue.config.productionTip = false
+Vue.prototype.global = global // 挂载全局配置模块
+
+Vue.use(ElementUI)
+Vue.use(api)  // 引入API模块
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  i18n,
   router,
+  store,
   render: h => h(App)
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,34 @@
 <template>
-  <div class="page">
-    <h2>Home Page</h2>
-    <el-button type="primary" @click="testAxios()">测试Axios调用</el-button>
-    <el-button type="primary" @click="getUser()">获取用户信息</el-button>
-    <el-button type="primary" @click="getMenu()">获取菜单信息</el-button>
+  <div class="container">
+      <!-- 导航菜单栏 -->
+      <nav-bar></nav-bar>
+      <!-- 头部区域 -->
+      <head-bar></head-bar>
+      <!-- 主内容区域 -->
+      <main-content></main-content>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-import mock from '@/mock/mock.js'
+import HeadBar from "./HeadBar"
+import NavBar from "./NavBar"
+import MainContent from "./MainContent"
 export default {
-  name: 'Home',
-  methods: {
-    testAxios() {
-      axios.get('http://localhost:8080').then(res => { alert(res.data) })
-    },
-    getUser() {
-      axios.get('http://localhost:8080/user').then(res => { alert(JSON.stringify(res.data)) })
-    },
-    getMenu() {
-      axios.get('http://localhost:8080/menu').then(res => { alert(JSON.stringify(res.data)) })
-    }
+  components:{
+        HeadBar,
+        NavBar,
+        MainContent
   }
-}
+};
 </script>
+
+<style scoped lang="scss">
+  .container {
+    position:absolute;
+    top: 0px;
+    left: 0px;
+    right: 0px;
+    bottom: 0px;
+    // background: rgba(224, 234, 235, 0.1);
+  }
+</style>  
